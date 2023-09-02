f117f51ca5ae66717d30f51072d9e6c8365dae24
jpablo79 <juanp_rgonzalez@hotmail.com>
2023-09-02 07:13:29 -0600

    Merge origin/master modificacion en el ejemplo herencia

--- src/herencia/ClsPruebaCuenta.java
+++ src/herencia/ClsPruebaCuenta.java
@@ -5,20 +5,24 @@ import java.util.*;
 public class ClsPruebaCuenta {
     
     public static void main(String args[]) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Cuenta de ahorros");
-    System.out.println("Ingrese saldo inicial= Q. ");
-        float saldoInicialAhorros = input.nextFloat();
-    System.out.print("Ingrese tasa de interés= ");
-        float tasaAhorros = input.nextFloat();
-        ClsCuentaAhorros cuenta1 = new ClsCuentaAhorros(saldoInicialAhorros, tasaAhorros);
-    System.out.print("Ingresar cantidad a consignar: Q. ");
-        float cantidadDepositar = input.nextFloat();
-        cuenta1.consignar(cantidadDepositar);
-    System.out.print("Ingresar cantidad a retirar: Q. ");
-        float cantidadRetirar = input.nextFloat();
-        cuenta1.retirar(cantidadRetirar);
-        cuenta1.extractoMensual();
-        cuenta1.imprimir();
+        Scanner input = new Scanner(System.in);
+            System.out.println("Cuenta de ahorros");
+            
+        System.out.println("Ingrese saldo inicial= Q. ");
+            float saldoInicialAhorros = input.nextFloat();
+            
+        System.out.print("Ingrese tasa de interés= ");
+            float tasaAhorros = input.nextFloat();
+             ClsCuentaAhorros cuenta1 = new ClsCuentaAhorros(saldoInicialAhorros, tasaAhorros);
+             
+        System.out.print("Ingresar cantidad a consignar: Q. ");
+            float cantidadDepositar = input.nextFloat();
+            cuenta1.consignar(cantidadDepositar);
+            
+        System.out.print("Ingresar cantidad a retirar: Q. ");
+            float cantidadRetirar = input.nextFloat();
+            cuenta1.retirar(cantidadRetirar);
+            cuenta1.extractoMensual();
+            cuenta1.imprimir();
     }    
 }
